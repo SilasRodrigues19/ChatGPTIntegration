ef53aca800ef9dac90d93afed66869c1eab0de0a
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2023-02-18 20:44:56 -0300

    refactor: add more environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,8 @@
 interface ImportMetaEnv {
   readonly PUBLIC_CHATGPT_API_KEY: string;
+  readonly OPENAI_MODEL: string;
+  readonly MAX_TOKENS: number;
+  readonly TEMPERATURE: number;
 }
 
 interface ImportMeta {
